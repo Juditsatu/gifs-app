aaa0556741245920fdcd7a2675b8814a37dc5edc
Judit Santos <judit.sant@gmail.com>
2022-07-12 12:56:23 +0200

    functional search and storage

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
+  
+  constructor(private gifsService: GifsService) { }
 
-  constructor() { }
+  get historial() {
+    return this.gifsService.historial;
+  }
 
-  ngOnInit(): void {
+  buscar(termino: string) {
+    this.gifsService.buscarGifs(termino);
   }
 
 }
